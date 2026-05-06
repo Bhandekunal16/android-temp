3b73bee68b6f049ad07536b4b7cc8772c6b5e0b5
bhandekunal16 <bhandekunal16@gmail.com>
2026-05-06 10:07:32 +0530

    `Added logging middleware and updated error handling`

--- server/app.js
+++ server/app.js
@@ -2,10 +2,18 @@ const express = require("express");
 const passwordManager = require("./password-manager/password-manager.service");
 const notesService = require("./notes/notes.service");
 const authService = require("./auth/auth.service");
+const Logger = require("log-byte");
 
 const app = express();
 app.use(express.json());
-
+app.use((req, res, next) => {
+  const { ip, method, body, url } = req;
+  Logger.info(`ip : ${ip}`);
+  Logger.info(`method : ${method}`);
+  Logger.info(`body : ${body}`);
+  Logger.info(`url : ${url}`);
+  next();
+});
 app.get("", (_, res) => {
   res.status(200).send("welcome in password-manager-server");
 });
@@ -46,10 +54,10 @@ app.post("/auth/save", async (req, res) => {
 });
 
 app.post("/auth/get", async (req, res) => {
-  const data = await new authService().get(req.body)
+  const data = await new authService().get(req.body);
   res.status(data.statusCode).send(data);
 });
 
 app.listen(3000, "0.0.0.0", (e) => {
-  e ? console.error(e.message) : console.log("http://0.0.0.0:3000");
+  e ? Logger.error(e.message) : Logger.info("http://0.0.0.0:3000");
 });
